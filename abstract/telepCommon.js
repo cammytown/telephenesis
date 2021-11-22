458f4191a9fa96d049ba45e9dec31a3ca42d48b6
Cameron Steele <cameronsteele@steelecameron.com>
2021-11-22 06:18:32 -0800

    improve page titles; make common between client and server

---
+++ abstract/telepCommon.js
@@ -0,0 +1,43 @@
+/**
+ * Get window/page title from page name.
+ * @param {string} pageName
+ * @param {Star} star
+ **/
+function getPageTitle(pageName, star) {
+	var siteTitle = "Telephenesis";
+	var pageTitle = "";
+
+	if(pageName) {
+		switch(pageName) {
+			case 'create': {
+				pageTitle = locale['CREATE-A-NEW-STAR'];
+			} break;
+
+			case 'recreate': {
+				pageTitle = locale['RECREATE-A-STAR'];
+			} break;
+
+			case 'star': {
+				//@TODO probably some kind of filtering
+				pageTitle = star.title;
+			} break;
+
+			default: {
+				console.warn("getPageTitle: unhandled page '" + pageName + "'");
+				// Capitalize:
+				pageTitle = pageName[0].toUpperCase() + pageName.substr(1);
+			}
+		}
+	}
+
+	if(pageTitle) {
+		return pageTitle + ' - ' + siteTitle;
+	} else {
+		return siteTitle;
+	}
+}
+
+module.exports = {
+	getPageTitle
+};
+
